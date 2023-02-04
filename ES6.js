97535ff83132440ac49ba6d26c61aba6af98dfe5
IbnuTopanAdib <resyaderesya@gmail.com>
2023-02-04 09:42:44 +0700

    a

--- ES6.js
+++ ES6.js
@@ -46,3 +46,27 @@ function removeFirstTwo(list) {
     return ({name, age, gender})
     // Only change code above this line
   };
+
+  // Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    }
+  };
+  // Only change code above this line
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
+
+  // Only change code below this line
+class Vegetable{
+    constructor(name){
+       this.name = name;
+    }
+  
+    
+  }
+  // Only change code above this line
+  
+  const carrot = new Vegetable('carrot');
+  console.log(carrot.name); // Should display 'carrot'
---
+++ constructor.js
@@ -0,0 +1,34 @@
+class Ibnu{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+const ibnu = new Ibnu('ibnu', 27)
+console.log(ibnu.name)
+console.log(ibnu.age)
+
+
+
+// Only change code below this line
+
+class Thermostat {
+    constructor(fahrenheit) {
+      this._fahrenheit = fahrenheit;
+    }
+    
+    get temperature() {
+      return (5 / 9) * (this._fahrenheit - 32);
+    }
+    
+    set temperature(celsius) {
+      this._fahrenheit = (celsius * 9.0) / 5 + 32;
+    }
+  }
+  
+  // Only change code above this line
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 in Celsius
